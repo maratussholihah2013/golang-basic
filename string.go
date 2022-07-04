35f21f239fbdd4e42fab801e09941369c4309cf0
maratussholihah2013 <maratus.sholihah.s.kom@gmail.com>
2022-07-04 11:50:21 +0700

    materi tipe data

---
+++ string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Maratus")
+	fmt.Println("Maratus Sholihah")
+
+	//menghitung jumlah karakter = len("string")
+	fmt.Println("Jumlah karakter = ", len("Maratus Sholihah"))
+	//mengambil byte dari karakternya string, contoh mengambil byte dari "t"
+	fmt.Println("Huruf ke 5 = ", "Maratus Sholihah"[4])
+
+}
